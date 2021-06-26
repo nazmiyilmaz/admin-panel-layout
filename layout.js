754be9f5bc35f40c40b6048d599a00ca3fdfff1f
nazmiyilmaz <nazmi.yilmaz@agu.edu.tr>
2021-06-26 11:46:11 +0300

    add dropdowns

--- layout.js
+++ layout.js
@@ -1,20 +1,49 @@
-const toggle = function () {
+const toggleMenu = function () {
+  hidePp()
+  hideNtf()
   document.getElementById('sidebar').classList.toggle('show')
   document.getElementById('btn-show').classList.toggle('btn-hide')
+  document.getElementById('menu-button').classList.toggle('mdi-menu')
+  document.getElementById('menu-button').classList.toggle('mdi-close')
 }
-
-const hide = function () {
+const hideMenu = function () {
   document.getElementById('sidebar').classList.remove('show')
   document.getElementById('btn-show').classList.remove('btn-hide')
+  document.getElementById('menu-button').classList.add('mdi-menu')
+  document.getElementById('menu-button').classList.remove('mdi-close')
 }
 
-document.getElementById('btn-show').addEventListener('click', toggle)
+const toggleNtf = function () {
+  hidePp()
+  hideMenu()
+  document.getElementById('ntf-dropdown').classList.toggle('hidden')
+}
+const hideNtf = function () {
+  document.getElementById('ntf-dropdown').classList.add('hidden')
+}
 
-document.getElementById('content').addEventListener('click', hide)
+const togglePp = function () {
+  hideNtf()
+  hideMenu()
+  document.getElementById('pp-dropdown').classList.toggle('hidden')
+}
+const hidePp = function () {
+  document.getElementById('pp-dropdown').classList.add('hidden')
+}
+
+// listeners
+document.getElementById('btn-show').addEventListener('click', toggleMenu)
+document.getElementById('content').addEventListener('click', () => {
+  hideMenu()
+  hideNtf()
+  hidePp()
+})
+document.getElementById('ntf').addEventListener('click', toggleNtf)
+document.getElementById('pp').addEventListener('click', togglePp)
 
 window.addEventListener('resize', () => {
   const widthOutput = document.querySelector('#width')
   if (widthOutput < 1000) {
-    hide()
+    hideMenu()
   }
 })
